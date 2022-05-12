d7ec40dbccb6c89685d2755ff1f41931f4954c7e
chris0512 <kyang3200@gmail.com>
2022-05-12 17:35:49 +0800

    parallel await

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 async function get3Pokemon() {
-	const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/1");
-	const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/2");
-	const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/3");
+	const prom1 =  axios.get("https://pokeapi.co/api/v2/pokemon/1");
+	const prom2 =  axios.get("https://pokeapi.co/api/v2/pokemon/2");
+	const prom3 =  axios.get("https://pokeapi.co/api/v2/pokemon/3");
+
+	console.log(prom1);
+
+	const poke1 = await prom1;
+	const poke2 = await prom2;
+	const poke3 = await prom3;
+
+
 	console.log(poke1.data);
 	console.log(poke2.data);
 	console.log(poke3.data);
@@ -9,3 +17,13 @@ async function get3Pokemon() {
 
 get3Pokemon();
 
+function changeBodyColor(color, delay) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			document.body.style.backgroundColor = color;
+			resolve();
+		}, delay);
+	});
+};
+
+changeBodyColor('teal', 1000);
